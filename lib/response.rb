10a5b2de665cd1465f56029668cec6651e9978f9
Chris Lind & Nolan Miller <student@epicodus.com>
2014-03-26 16:11:09 -0700

    checks for completed surveys

--- lib/response.rb
+++ lib/response.rb
@@ -2,4 +2,5 @@ class Response < ActiveRecord::Base
   belongs_to :taker
   belongs_to :question
   belongs_to :answer
+
 end
--- survey_ui.rb
+++ survey_ui.rb
@@ -31,7 +31,7 @@ end
 # ADMIN MENU -----------------------
 def admin_menu
   clear
-  puts "c: create survey, e: edit survey, x: return to main menu"
+  puts "c: create survey, e: edit survey, s: survey stats, x: return to main menu"
   case gets.chomp
   when 'c'
     create_survey
@@ -45,6 +45,31 @@ def admin_menu
     "Invalid"
     admin_menu
   end
+end
+
+def view_stats
+  Survey.all.each_with_index { |survey, index| puts "#{index + 1}.  #{survey.name}" }
+  puts "Enter a survey"
+  survey_choice = gets.chomp.to_i - 1
+  survey = Survey.find(Survey.all[survey_choice].id)
+  survey_questions = Question.all.where(survey_id: survey.id)
+  survey_questions.each do |q|
+    puts q.name
+    all_answers = []
+    survey_responses = Response.all.where(question_id: q.id)
+    survey_responses.each do |r|
+      all_answers << r.answer_id
+    end
+    unique_answers = survey_responses.select(:answer_id).distinct
+    answer_total = all_answers.length
+    # puts answer_total
+    unique_answers.each do |ua|
+      answer_count = all_answers.grep(ua.answer_id).size
+      answer_percentage = ((answer_count.to_f / answer_total.to_f) * 100).round
+      puts "\t#{Answer.find(ua.answer_id).name} (Total: #{answer_percentage}%)"
+    end
+  end
+  admin_menu
 end
 
 def create_survey
@@ -73,7 +98,6 @@ def add_answers
   answers = answers.gsub(/,\s/,',')
   answers = answers.split(",")
   answers.each do |answer|
-
     new_answer = Answer.create({:name => answer, :question_id => @current_question.id})
   end
   puts "Do you want to add another question (y/n)?"
@@ -119,13 +143,28 @@ end
 
 def list_surveys
   clear
+  puts "Completed Surveys:"
+  completed = Survey.joins(:takers).where(takers: {id: @current_taker.id})
+  # completed = Survey
+  completed.each {|survey| puts survey.name }
+
+  puts "Elligible Surveys:"
   Survey.all.each_with_index do |sur, index|
-    puts "#{sur.id}: #{sur.name}"
+    if not completed.pluck(:id).include?(sur.id)
+      puts "#{sur.id}: #{sur.name}"
+    end
   end
-  survey_choice = gets.chomp.to_i
-  survey = Survey.find(survey_choice)
-  survey.takers << @current_taker
-  take_survey(survey)
+  if Survey.all.length == completed.length
+    puts "No available surveys.  Come back later.  Press enter to return to main menu."
+    gets.chomp
+    main_menu
+  else
+    puts "Enter the id of the survey:"
+    survey_choice = gets.chomp.to_i
+    survey = Survey.find(survey_choice)
+    survey.takers << @current_taker
+    take_survey(survey)
+  end
 end
 
 def take_survey(survey)
@@ -144,7 +183,7 @@ def take_survey(survey)
     Response.create({:question_id => question.id, :answer_id => Answer.find(choice).id, :taker_id => @current_taker.id })
   end
   puts "Survey Complete"
-  list_surveys
+  user_menu
 end
 
 
